ebf158a411c284abdcdcff226d42848335bab40e
Gustavo <l.gustavo.gallo20@gmail.com>
2026-09-11 15:18:19 -0300

    first commit

---
+++ sistemaDeReservas/src/test/java/com/gustavo/sistemaDeReservas/SistemaDeReservasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavo.sistemaDeReservas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeReservasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
