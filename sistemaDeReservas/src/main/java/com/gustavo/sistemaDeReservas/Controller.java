c4ff689d6548b8ff2d5f28562c1daa1c02bcfe7a
Gustavo <l.gustavo.gallo20@gmail.com>
2026-09-13 19:05:04 -0300

    feat: adicao do metodo Controller

---
+++ sistemaDeReservas/src/main/java/com/gustavo/sistemaDeReservas/Controller.java
@@ -0,0 +1,15 @@
+package com.gustavo.sistemaDeReservas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasVindas")
+    public String boasVindas() {
+        return "Minha primeira mensagem";
+    }
+}
