269520cf4ab0336c1c9bb7e476f819e20d1be44b
Gustavo <l.gustavo.gallo20@gmail.com>
2026-09-14 10:53:27 -0300

    Revert "feat: Criação da entidade UsuarioModel"
    This reverts commit 31e7c42a6fdce0ab7a4c53f7363ecca484d33f8d.

--- sistemaDeReservas/src/main/java/com/gustavo/sistemaDeReservas/Controller.java
+++ sistemaDeReservas/src/main/java/com/gustavo/sistemaDeReservas/Controller.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping
-public class UsuarioController {
+public class Controller {
 
     @GetMapping("/boasVindas")
     public String boasVindas() {
