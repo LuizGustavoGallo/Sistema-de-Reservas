f2df1c20e0fcaf5e24627847ce3cb371611e2d70
Gustavo <l.gustavo.gallo20@gmail.com>
2026-09-18 12:13:12 -0300

    feat: adiciona busca de medico por especialidade

--- sistemaDeReservas/src/main/java/com/gustavo/sistemaDeReservas/Medico/MedicoRepository.java
+++ sistemaDeReservas/src/main/java/com/gustavo/sistemaDeReservas/Medico/MedicoRepository.java
@@ -2,5 +2,8 @@ package com.gustavo.sistemaDeReservas.Medico;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MedicoRepository extends JpaRepository<MedicoModel, Long> {
+    List<MedicoModel> findByEspecialidade(String especialidade);
 }
---
+++ sistemaDeReservas/src/main/java/com/gustavo/sistemaDeReservas/Medico/MedicoService.java
@@ -0,0 +1,19 @@
+package com.gustavo.sistemaDeReservas.Medico;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MedicoService {
+
+    private final MedicoRepository medicoRepository;
+
+    public MedicoService(MedicoRepository medicoRepository){
+        this.medicoRepository = medicoRepository;
+    }
+
+    public List<MedicoModel> buscaPorEspecialidade(String especialidade){
+        return medicoRepository.findByEspecialidade(especialidade);
+    }
+}
