df3259a6991966284cee2e5f87d27c00cdb14d0d
Gustavo <l.gustavo.gallo20@gmail.com>
2026-09-21 14:30:08 -0300

    feat: adiciona validacao de idade no paciente

--- sistemaDeReservas/src/main/java/com/gustavo/sistemaDeReservas/Paciente/PacienteModel.java
+++ sistemaDeReservas/src/main/java/com/gustavo/sistemaDeReservas/Paciente/PacienteModel.java
@@ -1,9 +1,7 @@
 package com.gustavo.sistemaDeReservas.Paciente;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.*;
 import lombok.*;
 
 @Entity
@@ -25,6 +23,9 @@ public class PacienteModel {
     @NotBlank(message = "Informe o Email")
     @Column(unique = true)
     private String email;
+
+    @Min(value = 0, message = "Idade não pode ser negativa.")
+    @Max(value = 120, message = "Idade inválida.")
     private int idade;
 
     @Pattern(regexp = "\\d{10,11}", message = "Informe um telefone válido")
